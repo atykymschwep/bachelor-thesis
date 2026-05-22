813f0584e384f4a9612e0fc930c3b681971dd457
atykymschwep <atykym.schwep@gmail.com>
2026-05-22 11:35:34 +0200

    Clean repo

--- build/odom_calib/CMakeFiles/odom_calib_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/odom_calib_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/odom_calib_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
